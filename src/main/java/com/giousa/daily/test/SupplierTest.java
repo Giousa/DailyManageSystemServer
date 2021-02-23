0ff35308160f30818e3cf58edf177e6209fddcfc
Giousa <65489469@qq.com>
2021-02-23 21:52:50 +0800

    dev

--- src/main/java/com/giousa/daily/test/SupplierTest.java
+++ src/main/java/com/giousa/daily/test/SupplierTest.java
@@ -18,6 +18,8 @@ public class SupplierTest {
 
         System.out.println("master 2021-02-23 21:51:48");
 
+        System.out.println("dev 2021-02-23 21:52:39");
+
 //        User user1 = new User("fff", 11);
 
         Supplier<User> user = () -> new User("aa",11);
