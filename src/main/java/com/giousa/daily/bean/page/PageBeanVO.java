c5eff7acc86f0b4d28997addf83e88e3e8d6602c
Giousa <65489469@qq.com>
2023-02-03 11:27:29 +0800

    删除无用数据

--- src/main/java/com/giousa/daily/bean/page/PageBeanVO.java
+++ src/main/java/com/giousa/daily/bean/page/PageBeanVO.java
@@ -1,105 +1,41 @@
 package com.giousa.daily.bean.page;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.List;
 
-/**
- * Description:
- * Author:zhangmengmeng
- * Date:2019/8/9
- * Email:65489469@qq.com
- */
 @Data
+@ToString
+@NoArgsConstructor
 public class PageBeanVO<T> implements Serializable {
 
-    private int totalCount;//总条目数
+    private static final long serialVersionUID = 1L;
 
-    private int totalPage;//总页数
+    /**
+     * /总条目数
+     */
+    private int totalCount;
 
-    private int pageNum;//当前页
+    /**
+     * 总页数
+     */
+    private int totalPage;
 
-    private int pageSize;//当前页显示条目数
+    /**
+     * 当前页
+     */
+    private int pageNum;
 
+    /**
+     * 当前页显示条目数
+     */
+    private int pageSize;
+
+    /**
+     * 数据内容
+     */
     private List<T> data;
-
-    public PageBeanVO() {
-    }
-
-    public PageBeanVO(int _pageNum, int _pageSize) {
-        this.pageNum = _pageNum;
-        this.pageSize = _pageSize;
-    }
-
-    public PageBeanVO(int _totalCount, int _pageNum, int _pageSize) {
-        this.pageNum = _pageNum;
-        this.pageSize = _pageSize;
-        this.totalCount = _totalCount;
-        if(_pageSize > 0 && _totalCount > 0){
-            if(totalCount%pageSize>0){
-                this.totalPage = totalCount/pageSize +  1;
-            }else {
-                this.totalPage = totalCount/pageSize;
-            }
-        }
-
-
-    }
-    public PageBeanVO(int _totalCount, int _totalPage, int _pageNum, int _pageSize) {
-        this.totalCount = _totalCount;
-        this.totalPage = _totalPage;
-        this.pageNum = _pageNum;
-        this.pageSize = _pageSize;
-    }
-
-    public PageBeanVO(int _totalCount, int _totalPage, int _pageNum, int _pageSize, List<T> _data) {
-        this.totalCount = _totalCount;
-        this.totalPage = _totalPage;
-        this.pageNum = _pageNum;
-        this.pageSize = _pageSize;
-        this.data = _data;
-    }
-
-
-
-    public int getTotalCount() {
-        return totalCount;
-    }
-
-    public void setTotalCount(int totalCount) {
-        this.totalCount = totalCount;
-    }
-
-    public int getTotalPage() {
-        return totalPage;
-    }
-
-    public void setTotalPage(int totalPage) {
-        this.totalPage = totalPage;
-    }
-
-    public int getPageNum() {
-        return pageNum;
-    }
-
-    public void setPageNum(int pageNum) {
-        this.pageNum = pageNum;
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public List<T> getData() {
-        return data;
-    }
-
-    public void setData(List<T> data) {
-        this.data = data;
-    }
 }
